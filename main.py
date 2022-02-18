fc250c8668755e0e6e9202e7979542e60f172b67
ChatDo <nicolas.julie@epitech.eu>
2022-02-18 16:48:54 +0400

    :tada: starting first neuron

---
+++ main.py
@@ -0,0 +1,38 @@
+from operator import mul
+import numpy as np
+from util import *
+
+        # STEP 1 #
+# my_matrix = np.array([1, 2, 3])
+# create_first_matrix(my_matrix)
+        # STEP 1 #
+
+
+
+        # STEP 2 #
+# b = my_new_array()
+# print(b.shape)
+        # STEP 2 #
+
+
+
+        # STEP 3 #
+# a1 = np.array([[2, 2],
+#                 [0, 1]])
+# a2 = np.array([[4, 4, 4, 5],
+#                 [0, 1, 6, 5],
+#                 [0, 1, 3, 6],
+#                 [0, 1, 1, 0]])
+# a3 = np.array([[2, 2],
+#                 [0, 1],
+#                 [2, 2],
+#                 [0, 1],
+#                 [5, 8]])
+# check_random_matrix(a1, a2, a3)
+        # STEP 3 #
+
+
+        # STEP 4 #
+# a4 = np.matmul(a3, a1)
+# check_mul(a4)
+        # STEP 4 #
